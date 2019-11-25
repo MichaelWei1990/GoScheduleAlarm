13c568393db317f86e374154f5ae642d8967a454
MichaelWei1990 <michaelwei0901@gmail.com>
2019-11-24 22:59:05 -0500

    Modified Stop class and its test class.

--- core/model/stopTest.py
+++ core/model/stopTest.py
@@ -7,19 +7,23 @@ class TestStop(unittest.TestCase):
     def setUp(self):
         print("setUp")
         self.stopMountJoy = Stop("MJ", "Mount Joy GO", "https://www.gotransit.com/en/stations-stops-parking/find-a-station-or-stop/results?stationCode=MJ")
+        self.stopUnionStation = Stop("UN", "Union Station", "https://www.gotransit.com/en/stations-stops-parking/find-a-station-or-stop/results?stationCode=UN")
 
 
     def test_id(self):
         print("test_id")
         self.assertEqual(self.stopMountJoy.get_id(), "MJ")
+        self.assertEqual(self.stopUnionStation.get_id(), "UN")
 
     def test_name(self):
         print("test_name")
         self.assertEqual(self.stopMountJoy.get_name(), "Mount Joy GO")
+        self.assertEqual(self.stopUnionStation.get_name(), "Union Station")
 
     def test_url(self):
         print("test_url")
         self.assertEqual(self.stopMountJoy.get_url(), "https://www.gotransit.com/en/stations-stops-parking/find-a-station-or-stop/results?stationCode=MJ")
+        self.assertEqual(self.stopUnionStation.get_url(), "https://www.gotransit.com/en/stations-stops-parking/find-a-station-or-stop/results?stationCode=UN")
     
 if __name__ == '__main__':
     unittest.main()
